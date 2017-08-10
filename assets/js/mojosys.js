ae18621465b1637986dabedb7c47eac99c538558
Vikram K Ponnoju <vikram.ponnoju@gmail.com>
2017-08-11 00:34:59 +0530

    Feature to send email from contact form

--- assets/js/mojosys.js
+++ assets/js/mojosys.js
@@ -1,11 +1,29 @@
 function sendMessage()
 {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+    document.getElementById("confirmation").innerHTML = "Sending email..";
+    var from_name = document.getElementById("name").value;
+    var from_email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    //SEND THIS INFORMATION TO AN API
-    //alert(name + "-" + email);
-    //alert(message);
-    document.getElementById("confirmation").innerHTML = "Thank you " + name + ", for writing to us. We will get back to you in no time!"; 
+
+    var email_template = "mojosys_email_template";
+    var email_service_id = "zoho";
+    var user_id = "user_cIqfxVYy4geTxHbHmGh4n";
+
+    emailjs.init(user_id);
+
+    emailjs.send(email_service_id, email_template,{"from_email": from_email, "message_html": message, "from_name" : from_name})
+    .then(function(response) {
+        console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        document.getElementById("confirmation").innerHTML = "Thank you " + from_name + ", for writing to us. We will get back to you in no time!"; 
+       
+}, function(err) {
+         document.getElementById("confirmation").innerHTML = from_name + ", Something wrent wrong at our side. Please drop an email to "
+        + " <a href='mailto:contact@mojosys.com?subject=Message to Mojosys'>contact@mojosys.com</a>";
+});
     return false;
+}
+
+
+function onbodyLoad() {
+    document.getElementById("confirmation").innerHTML = "";
 }
