21eb8bac8087d8075ed2c0050524fae9bc7f277b
Vikram K Ponnoju <vikram.ponnoju@gmail.com>
2017-11-10 17:07:07 +0530

    addition of mailchimp subscriber popup

---
+++ assets/js/mailchimp.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us16.list-manage.com","uuid":"332482e63df2e15473c70027e","lid":"14032a2fa8"}) })</script>
